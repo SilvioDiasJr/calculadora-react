31a270c32f8eafb3cb4b5b5a498dc1f7c8de5975
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-27 16:00:46 -0300

    criar projeto Calculadora

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Calculator.css'
+
+const Calculator = () => {
+  return (
+    <div className="calculator">
+    
+    </div>
+  )
+}
+
+export default Calculator
