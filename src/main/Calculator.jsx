5212a78db043e8af49ea0dda8bac513cfc6a041a
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-27 17:32:00 -0300

    adicionar funções

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -5,26 +5,39 @@ import Display from '../components/Display'
 import './Calculator.css'
 
 const Calculator = () => {
+
+  function clearMemory() {
+    console.log('limpar')
+  }
+
+  function setOperation(operation) {
+    console.log(operation)
+  }
+
+  function addDigit(n) {
+    console.log(n)
+  }
+
   return (
     <div className="calculator">
-      <Display value={100}/>
-      <Button label='AC'/>
-      <Button label='/'/>
-      <Button label='7'/>
-      <Button label='8'/>
-      <Button label='9'/>
-      <Button label='*'/>
-      <Button label='4'/>
-      <Button label='5'/>
-      <Button label='6'/>
-      <Button label='-'/>
-      <Button label='1'/>
-      <Button label='2'/>
-      <Button label='3'/>
-      <Button label='+'/>
-      <Button label='0'/>
-      <Button label='.'/>
-      <Button label='='/>
+      <Display value={100} />
+      <Button label='AC' click={_ => clearMemory()} triple/>
+      <Button label='/' click={_ => setOperation('/')} operation/>
+      <Button label='7' click={_ => addDigit('7')}/>
+      <Button label='8' click={_ => addDigit('8')}/>
+      <Button label='9' click={_ => addDigit('9')}/>
+      <Button label='*' click={_ => setOperation('*')} operation/>
+      <Button label='4' click={_ => addDigit('4')}/>
+      <Button label='5' click={_ => addDigit('5')}/>
+      <Button label='6' click={_ => addDigit('6')}/>
+      <Button label='-' click={_ => setOperation('-')} operation/>
+      <Button label='1' click={_ => addDigit('1')}/>
+      <Button label='2' click={_ => addDigit('2')}/>
+      <Button label='3' click={_ => addDigit('3')}/>
+      <Button label='+' click={_ => setOperation('+')} operation/>
+      <Button label='0' click={_ => addDigit('0')} double/>
+      <Button label='.' click={_ => addDigit('.')}/>
+      <Button label='=' click={_ => setOperation('=')} operation/>
     </div>
   )
 }
